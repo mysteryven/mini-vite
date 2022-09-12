be68fc195c6ab5a56835c2f8b641d21023167d8e
wenzhe <mysteryven@gmail.com>
2022-09-12 17:12:32 +0800

    test: static server demo

--- packages/demo/src/index.ts
+++ packages/demo/src/index.ts
@@ -1,8 +1,13 @@
+import './index.css'
+import './App.css'
+
 const render = () => {
     const root = document.querySelector('#root')!
+    const img = new Image()
+    img.src = '/vite.svg'
 
     root.innerHTML = "Hello, I am from TS"
-
+    root.appendChild(img)
 }
 render()
 
